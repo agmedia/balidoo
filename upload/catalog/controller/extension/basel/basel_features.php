1faf979e456fa48f38f4e7cc943b5734e1966c38
tomashina <tomislav@agmedia.hr>
2025-10-03 10:49:22 +0200

    push

--- upload/catalog/controller/extension/basel/basel_features.php
+++ upload/catalog/controller/extension/basel/basel_features.php
@@ -496,9 +496,9 @@ class ControllerExtensionBaselBaselFeatures extends Controller {
         }
 
         // --- Mailchimp config iz tvog configa ---
-        $apiKey = '692f7811c353921ffb1f47cfde379f5f-us22';   // npr. 4569...-us18
-        $server = 'us22';    // "us18"
-        $listId = '0a99e9ec55';   // "5d2b9f4871"
+        $apiKey = agconf('mailchimp.api_key');   // npr. 4569...-us18
+        $server = agconf('mailchimp.server');    // "us18"
+        $listId = agconf('mailchimp.list_id');   // "5d2b9f4871"
 
         // Fallback: ako server nije postavljen, izvuci ga iz API key-a
         if (!$server && strpos($apiKey, '-') !== false) {
@@ -550,12 +550,12 @@ class ControllerExtensionBaselBaselFeatures extends Controller {
             $mail->smtp_port     = $this->config->get('config_mail_smtp_port');
             $mail->smtp_timeout  = $this->config->get('config_mail_smtp_timeout');
 
-            $mail->setTo($email);
+           /* $mail->setTo($email);
             $mail->setFrom(agconf('mailchimp.newsletter.from'));
             $mail->setSender(html_entity_decode(agconf('mailchimp.newsletter.title'), ENT_QUOTES, 'UTF-8'));
             $mail->setSubject(html_entity_decode(sprintf(agconf('mailchimp.newsletter.subject'), agconf('mailchimp.newsletter.title')), ENT_QUOTES, 'UTF-8'));
             $mail->setHtml(agconf('mailchimp.newsletter.html'));
-            $mail->send();
+            $mail->send();*/
 
             $json['success'] = agconf('mailchimp.newsletter.success');
             $json['color']   = '#DAFFCA';
